8d58a7e8710721005b2ee6deb40627136da76343
AKIRA-MIYAKE <akira.aktk.miyake@gmail.com>
2023-08-03 16:56:01 +0900

    Render asset image on PostContent

--- src/components/posts/PostContent/index.tsx
+++ src/components/posts/PostContent/index.tsx
@@ -1,5 +1,9 @@
 import type { FC } from 'react';
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import {
+  documentToReactComponents,
+  Options,
+} from '@contentful/rich-text-react-renderer';
+import { BLOCKS } from '@contentful/rich-text-types';
 
 import type { Post } from '@/interfaces';
 
@@ -9,10 +13,32 @@ export interface PostContentProps {
   content: Post['content'];
 }
 
+const customMarkdownOptions: (content: Post['content']) => Options = (
+  content,
+) => {
+  return {
+    renderNode: {
+      [BLOCKS.EMBEDDED_ASSET]: (node) => {
+        const asset = content.links.assets.block.find(
+          (asset) => asset.sys.id === node.data.target.sys.id,
+        );
+
+        if (!asset) {
+          return null;
+        }
+
+        return (
+          <img src={asset.url} alt={asset.description} className="w-full" />
+        );
+      },
+    },
+  };
+};
+
 export const PostContent: FC<PostContentProps> = ({ content }) => {
   return (
     <div className={styles['content']}>
-      {documentToReactComponents(content.json)}
+      {documentToReactComponents(content.json, customMarkdownOptions(content))}
     </div>
   );
 };
